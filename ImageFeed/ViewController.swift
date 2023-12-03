8217636e33ca4fc76247ebdee5f6fe9dda698291
lomakina-mariya <145546282+lomakina-mariya@users.noreply.github.com>
2023-12-03 18:32:04 +0300

    Initial Commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Masha-MBP on 03.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
